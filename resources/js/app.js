349e57d272b4f95c993ccd16ee1db5e0177c3024
Abraão Ribeiro <abraaobeiro@gmail.com>
2025-09-21 09:29:41 -0300

    feat(ui): modal transaction

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
-import './globals/theme.js'; /* By Sheaf.dev */ 
-
+import './globals/theme.js';
+import './globals/modals.js';
 import './bootstrap';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 
 // now you can register
 // components using Alpine.data(...) and
-// plugins using Alpine.plugin(...) 
+// plugins using Alpine.plugin(...)
 
 
- 
+
 Livewire.start()
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,6 +1,8 @@
 <div class="grid gap-4 md:gap-8 pb-8 px-4">
     <x-app.header title="Dashboard" description="Visão geral das suas finanças">
-        <x-ui.button icon="plus">Nova Transação</x-ui.button>
+        <x-ui.modal.trigger id="transactionModal">
+            <x-ui.button icon="plus">Nova Transação</x-ui.button>
+        </x-ui.modal.trigger>
     </x-app.header>
 
     <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
@@ -38,4 +40,88 @@
             @endif
         </div>
     </div>
+    <x-ui.modal
+        id="transactionModal"
+        width="lg"
+        position="center"
+        heading="Nova Transação"
+        description="Preencha os detalhes da nova transação abaixo"
+    >
+        <x-ui.field class="mb-4">
+            <x-ui.label>Nome</x-ui.label>
+            <x-ui.input wire:model="name" placeholder="Ex: Salário, Aluguel, etc."/>
+            <x-ui.error name="name" />
+        </x-ui.field>
+
+        <x-ui.field class="mb-4">
+            <x-ui.label>Valor</x-ui.label>
+            <x-ui.input wire:model="amount" placeholder="R$ 0,00" x-ref="money"
+                        x-on:input="
+            let value = $refs.money.value.replace(/\D/g, '');
+            value = (value / 100).toFixed(2) + '';
+            value = value.replace('.', ',');
+            value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+            $refs.money.value = 'R$ ' + value;
+        "/>
+            <x-ui.error name="amount" />
+        </x-ui.field>
+
+        <x-ui.radio.group wire:model="type" label="Tipo" variant="segmented" direction="horizontal" class="mb-4">
+            <x-ui.radio.item
+                value="Receita"
+                label="Receita"
+            />
+            <x-ui.radio.item
+                value="Despesa"
+                label="Despesa"
+            />
+        </x-ui.radio.group>
+
+        <x-ui.field class="mb-4">
+            <x-ui.label>Categoria</x-ui.label>
+            <x-ui.select
+                placeholder="Selecione uma categoria"
+                wire:model="category">
+                <x-ui.select.option value="option1">Option 1</x-ui.select.option>
+                <x-ui.select.option value="option2">Option 2</x-ui.select.option>
+            </x-ui.select>
+            <x-ui.error name="category" />
+        </x-ui.field>
+
+        <x-ui.field class="mb-4">
+            <x-ui.label>Cartão de crédito (opcional)</x-ui.label>
+            <x-ui.select
+                placeholder="Selecione um cartão"
+                wire:model="card">
+                <x-ui.select.option value="option1">Option 1</x-ui.select.option>
+                <x-ui.select.option value="option2">Option 2</x-ui.select.option>
+            </x-ui.select>
+            <x-ui.error name="card" />
+        </x-ui.field>
+
+        <x-ui.field class="mb-4">
+            <x-ui.label>Data</x-ui.label>
+            <x-ui.input type="date" wire:model="date"/>
+            <x-ui.error name="date" />
+        </x-ui.field>
+
+        <x-ui.field class="mb-4">
+            <x-ui.label>Descrição (opcional)</x-ui.label>
+            <x-ui.textarea
+                wire:model="description"
+                placeholder="Descrição..."
+            />
+        </x-ui.field>
+
+        <x-slot name="footer">
+            <div class="w-full flex justify-end space-x-3">
+                <x-ui.button x-on:click="$data.close();" variant="outline">
+                    Cancelar
+                </x-ui.button>
+                <x-ui.button x-on:click="$data.close();" variant="primary">
+                    Salvar
+                </x-ui.button>
+            </div>
+        </x-slot>
+    </x-ui.modal>
 </div>
